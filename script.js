bdc2a8d2391a8a107866d75d9d810dc7bfbc50cc
Murad Hasan <muradhasanshuco00@gmail.com>
2026-01-26 23:26:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// let arr = ["mengo","apple","grave","jackfoot"]
+// let arr2 = [22,33,44,55,66,77,88,99,]
+
+
+// for(const newArray of arr){
+//     console.log(newArray)
+// }
+
+// for( const newArray2 of arr2){
+//     let a = newArray2 % 2
+//     console.log(a)
+// }
+
+let number = 1;
+
+while (number < 10) {
+    number++
+    console.log(number)
+}
