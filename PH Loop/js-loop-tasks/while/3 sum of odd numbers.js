132f640e8ada0c36a3c0611f0be1e1402e3c9bdd
Murad Hasan <muradhasanshuco00@gmail.com>
2026-01-27 00:13:04 +0600

    ans no3 problem

--- PH Loop/js-loop-tasks/while/3 sum of odd numbers.js
+++ PH Loop/js-loop-tasks/while/3 sum of odd numbers.js
@@ -16,3 +16,33 @@ Display sum of all the even numbers from 206 to 311.
  */
 
 /*programming hero*/
+
+
+// subtask 1 ans:
+
+let oddNumber = 81;
+let oddSum = 0;
+
+while (oddNumber <= 131) {
+    if (oddNumber % 2 == 1) {
+        oddSum = oddSum + oddNumber
+        console.log("oddNumber sum is :", oddSum)
+    }
+
+    oddNumber++
+
+}
+
+// subtask 2 ans:
+
+let evenNumer = 206;
+let evenNumerSum = 0;
+
+while (evenNumer <= 311) {
+    if (evenNumer % 2 == 0) {
+        evenNumerSum = evenNumerSum + evenNumer;
+        console.log("The even sum numbers is :", evenNumerSum)
+
+    }
+    evenNumer++
+}
