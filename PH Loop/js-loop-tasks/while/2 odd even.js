a135566eff76fed9a61c1dd13f0090ebc21b4cfe
Murad Hasan <muradhasanshuco00@gmail.com>
2026-01-26 23:49:23 +0600

    ans subtask 1 and 2

--- PH Loop/js-loop-tasks/while/2 odd even.js
+++ PH Loop/js-loop-tasks/while/2 odd even.js
@@ -16,3 +16,29 @@ Find all the even numbers from 78 to 98.
  */
 
 /*programming hero*/
+
+// Subtask 1 Ans
+
+let subNumber1 = 78;
+while (subNumber1 <= 98) {
+    // console.log(subNumber1)
+    if (subNumber1 % 2 == 1) {
+        console.log("The odd numbers :", subNumber1)
+    }
+    subNumber1++
+}
+
+console.log("")
+console.log("")
+console.log("")
+
+// Subtask 2 Ans
+
+let subNumber2 = 61;
+while (subNumber2 <= 100) {
+    // console.log(subNumber2)
+    if (subNumber2 % 2 == 0) {
+        console.log("The even numbers :", subNumber2)
+    }
+    subNumber2++
+}
