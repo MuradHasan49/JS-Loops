6621417a8f00472a6aaf165dfe5fbb9b6f769f66
Murad Hasan <muradhasanshuco00@gmail.com>
2026-01-28 12:39:35 +0600

    practice some new topic

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-// let arr = ["mengo","apple","grave","jackfoot"]
-// let arr2 = [22,33,44,55,66,77,88,99,]
+// // let arr = ["mengo","apple","grave","jackfoot"]
+// // let arr2 = [22,33,44,55,66,77,88,99,]
 
 
-// for(const newArray of arr){
-//     console.log(newArray)
+// // for(const newArray of arr){
+// //     console.log(newArray)
+// // }
+
+// // for( const newArray2 of arr2){
+// //     let a = newArray2 % 2
+// //     console.log(a)
+// // }
+
+// let number = 1;
+
+// while (number < 10) {
+//     number++
+//     console.log(number)
 // }
 
-// for( const newArray2 of arr2){
-//     let a = newArray2 % 2
-//     console.log(a)
-// }
 
-let number = 1;
+// string and object 
 
-while (number < 10) {
-    number++
-    console.log(number)
-}
+let name = 'bangladesh'
+ name = name[2];
+console.log(typeof name , name)
+
+
+let arry = [ 22, 44, 55, 66, 88 ];
+
+console.log( arry[2])
+
+arry [2] = 100;
+
+console.log(arry)
