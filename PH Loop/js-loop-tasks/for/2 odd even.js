51bf958d0fcff212d88088ba838dc02ca6eac6f0
Murad Hasan <muradhasanshuco00@gmail.com>
2026-01-29 22:59:58 +0600

    solve for 2 Problem

--- PH Loop/js-loop-tasks/for/2 odd even.js
+++ PH Loop/js-loop-tasks/for/2 odd even.js
@@ -5,8 +5,6 @@ Subtask-1:
 Find all the odd numbers from 61 to 100.
 
  */
-
-
 /***
 
 Subtask-2:
@@ -16,3 +14,24 @@ Find all the even numbers from 78 to 98.
  */
 
 /*programming hero*/
+
+let oddNum = 100;
+
+for (let i = 61; i <= oddNum; i++) {
+    // console.log(i)
+    if (i % 2 === 1) {
+        // console.log(i)
+    }
+}
+
+
+let evenNum = 98;
+
+for (let j = 78; j <= evenNum; j++) {
+    // console.log(j)
+    if (j % 2 === 0) {
+        console.log(j)
+
+    }
+
+}
