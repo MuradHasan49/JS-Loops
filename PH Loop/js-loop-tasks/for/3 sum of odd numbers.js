55a678c3b766a84366775692841645d9aa9b7cd2
Murad Hasan <muradhasanshuco00@gmail.com>
2026-01-29 23:15:03 +0600

    for 3 solve

--- PH Loop/js-loop-tasks/for/3 sum of odd numbers.js
+++ PH Loop/js-loop-tasks/for/3 sum of odd numbers.js
@@ -16,3 +16,25 @@ Display sum of all the even numbers from 51 to 85.
  */
 
 /*programming hero*/
+
+let sumOddNumber = 129 ;
+let sumOfOdd = 0;
+
+for( let i = 91; i <= sumOddNumber ; i++){
+    // console.log(i)
+    if( i % 2 === 1){
+        sumOfOdd = sumOfOdd + i;
+    }
+}
+console.log(sumOfOdd)
+
+
+let num = 100;
+let newSum = 0 ;
+for( let j = 51; j <= num ; j++){
+    if( j % 2 === 0 ){
+        newSum = newSum + j;
+    }
+    
+}
+console.log(newSum)
