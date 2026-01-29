6eb7d0da153559a1877738252554c93f838ae34f
Murad Hasan <muradhasanshuco00@gmail.com>
2026-01-29 22:42:32 +0600

    sovle while 5

--- script.js
+++ script.js
@@ -1,59 +1,60 @@
-// // // let arr = ["mengo","apple","grave","jackfoot"]
-// // // let arr2 = [22,33,44,55,66,77,88,99,]
+// // // // let arr = ["mengo","apple","grave","jackfoot"]
+// // // // let arr2 = [22,33,44,55,66,77,88,99,]
 
 
-// // // for(const newArray of arr){
-// // //     console.log(newArray)
+// // // // for(const newArray of arr){
+// // // //     console.log(newArray)
+// // // // }
+
+// // // // for( const newArray2 of arr2){
+// // // //     let a = newArray2 % 2
+// // // //     console.log(a)
+// // // // }
+
+// // // let number = 1;
+
+// // // while (number < 10) {
+// // //     number++
+// // //     console.log(number)
 // // // }
 
-// // // for( const newArray2 of arr2){
-// // //     let a = newArray2 % 2
-// // //     console.log(a)
-// // // }
 
-// // let number = 1;
+// // // string and object 
 
-// // while (number < 10) {
-// //     number++
-// //     console.log(number)
-// // }
+// // let name = 'bangladesh'
+// //  name = name[2];
+// // console.log(typeof name , name)
 
 
-// // string and object 
+// // let arry = [ 22, 44, 55, 66, 88 ];
 
-// let name = 'bangladesh'
-//  name = name[2];
-// console.log(typeof name , name)
+// // console.log( arry[2])
 
+// // arry [2] = 100;
 
-// let arry = [ 22, 44, 55, 66, 88 ];
-
-// console.log( arry[2])
-
-// arry [2] = 100;
-
-// console.log(arry)
+// // console.log(arry)
 
 
-// case in javascript 
+// // case in javascript 
 
-let email = 'Muradhasanshuvo'
-let email2 = 'muradhasanshuvo'
+// let email = 'Muradhasanshuvo'
+// let email2 = 'muradhasanshuvo'
 
-if (email.toLowerCase === email2.toLowerCase) {
-    console.log("enter your password ")
-} else{
-    console.log("wrong email addres")
-}
+// if (email.toLowerCase === email2.toLowerCase) {
+//     console.log("enter your password ")
+// } else{
+//     console.log("wrong email addres")
+// }
 
-// if white space
+// // if white space
 
 
-let useremail = '  muradhasanshuvo'
-let useremail2 = '  muradhasanshuvo  '
+// let useremail = '  muradhasanshuvo'
+// let useremail2 = '  muradhasanshuvo  '
 
-if (useremail.trim === useremail2.trim) {
-    console.log("enter your password ")
-} else{
-    console.log("wrong email addres")
-}
+// if (useremail.trim === useremail2.trim) {
+//     console.log("enter your password ")
+// } else{
+//     console.log("wrong email addres")
+// }
+
