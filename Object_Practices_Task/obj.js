e7ba6a9ded9a56070a4a31c81df9a8c4bb4fe768
Murad Hasan <muradhasanshuco00@gmail.com>
2026-02-02 01:57:46 +0600

    complete reverse task

--- Object_Practices_Task/obj.js
+++ Object_Practices_Task/obj.js
@@ -1,53 +1,53 @@
-// // Task 1
+// Task 1
 
-// const colors = {
-//     red: "#ff0000",
-//     green: "#00ff00",
-//     blue: "#0000ff",
-//     "golden rod": '#daa520'
-// };
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
 
-// console.log(colors["golden rod"])
-
-
-// // Task 2 
-
-// const car = {
-//     make: "Toyota",
-//     model: "Corolla",
-//     year: 2020,
-//     "passenger capacity" : 5
-// };
-// console.log(car["passenger capacity"])
+console.log(colors["golden rod"])
 
 
-// // Task 3 
+// Task 2 
 
-// const student = {
-//     name: "Hamim Sakep",
-//     id: 5421,
-//     physics: {
-//         subject: "HSC Physics",
-//         author: "Shahjahan Tapan",
-//         marks: 30
-//     }
-// };
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    "passenger capacity" : 5
+};
+console.log(car["passenger capacity"])
 
-// console.log(student["physics"].marks)
+        
+// Task 3 
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+
+console.log(student["physics"].marks)
 
 
 // Task 4
 
-// let allStudent = {
-//     name: 'Ariana Grande',
-//     age: 21,
-//     city: 'Gaibandha',
-//     isStudent: true
-// };
+let allStudent = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
 
-// let aLArr = Object.keys(allStudent)
+let aLArr = Object.keys(allStudent)
 
-// console.log(aLArr.length)
+console.log(aLArr.length)
 
 
 // Task 5
---
+++ Object_Practices_Task/reverse.js
@@ -0,0 +1,26 @@
+let arry = [ "mark","jhon","MHS","harry","shubam"];
+let arry2 = [0,1,2,3,4,5,6,7,8,9,10]
+
+// let r = arry.reverse();
+// console.log(r)
+// let r2 = arry2.reverse();
+// console.log(r2)
+
+// let a = []
+// for(let i of arry){
+//     a.unshift(i)
+// }
+// console.log(a)
+
+// let a2 = []
+// for(let i of arry2){
+//     a2.unshift(i)
+// }
+// console.log(a2)
+
+// for( let i = arry2.length -1; i >= 0; i--){
+//     console.log(i)
+// }
+// for( let i = arry.length -1; i >= 0; i--){
+//     console.log(arry[i])
+// }
