86a7ee5f25eee1407cd6d441f906d21f1cb905e6
Murad Hasan <muradhasanshuco00@gmail.com>
2026-02-03 00:45:37 +0600

    complete task 3

--- String_Practice_Task/Str_problem.js
+++ String_Practice_Task/Str_problem.js
@@ -33,6 +33,19 @@ for( let i of testStr.toUpperCase()){
 
 }
 
-console.log("Total Charecter is :",total.length)
+// console.log("Total Charecter is :",total.length)
+
+
+// Task 3
+// Check whether a string contains all the vowels a, e, i, o, u
+let vowelsStr = "i am edit some video of us gov."
+// let vowelsStr = "Hymn rhythms fly. My spry nymph, try my gym. Why dry? Cry, sly sky. By thy myth, fly. My shy lynx, cry by my sky."
+
+if(vowelsStr.includes("a") || vowelsStr.includes("e") || vowelsStr.includes("i") || vowelsStr.includes("o") || vowelsStr.includes("u")){
+    console.log("Yes Vowels is Here  ")
+}else{
+    console.log("Sorry no Vowels Here")
+
+}
 
 
