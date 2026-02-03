f2b94713caff5d9b824787e6616228ac8cc10d7e
Murad Hasan <muradhasanshuco00@gmail.com>
2026-02-04 00:47:22 +0600

    no more duplicate item in array

--- even_ave.js
+++ even_ave.js
@@ -1,16 +1,20 @@
-let arry = [22,1,3,45,6,6,7,54,3,57,57,5,87,44,88,99,34,8,2,200,600];
+let arry = [222,1,3,45,6,6,7,54,3,57,57,5,87,44,88,99,34,8,2,200,600,22,6,44];
 
 function evenAve(number){
     let evenSum = 0;
     let evenLength = [];
+    let removeDuplicate = [];
     for(let num of number){
-        if( num % 2 === 0){
+        if( num % 2 === 0 && removeDuplicate.includes(num) === false){
             evenSum = evenSum + num;
             evenLength.push(num)
+            removeDuplicate.push(num)
         }
     }
-    let result = evenSum / evenLength.length
+
+    let result = evenSum / evenLength.length;
     return result
+    
 }
 
 let output = evenAve(arry)
