8c338c8498f1d7468fa9f687c4c85b09a436d57d
Murad Hasan <muradhasanshuco00@gmail.com>
2026-02-05 17:46:02 +0600

    fix some but of this code

--- Array-Looping-Task/array_task.js
+++ Array-Looping-Task/array_task.js
@@ -4,7 +4,7 @@ const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
 
 function revArray (input){
     let storeArray = []
-    for(let j = colors.length - 1 ; j >= 0 ; j --){
+    for(let j = input.length - 1 ; j >= 0 ; j --){
         storeArray.push(colors[j])
     }
     return storeArray;
