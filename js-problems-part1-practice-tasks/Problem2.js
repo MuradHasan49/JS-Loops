c6abdd2674f60902a2932e901c6ac7e39175de43
Murad Hasan <muradhasanshuco00@gmail.com>
2026-02-07 23:19:36 +0600

    problem 2 solve

--- js-problems-part1-practice-tasks/Problem2.js
+++ js-problems-part1-practice-tasks/Problem2.js
@@ -1,17 +1,17 @@
-let num =  [5,6,11,12,98, 5]
+let num = [5, 6, 11, 12, 98, 5]
 let r = []
-for( let n of num){
-    if( n === 5 ){
-    r.push(n)
+for (let n of num) {
+    if (n === 5) {
+        r.push(n)
 
-}
+    }
 }
 console.log(r.length)
 
-let num2 = numbers = [5,6,11,12,98, 5]
+let num2 = numbers = [5, 6, 11, 12, 98, 5]
 let r2 = []
-for( let i of numbers){
-    if( i === 25){
+for (let i of numbers) {
+    if (i === 25) {
         r2.push(i)
     }
 }
