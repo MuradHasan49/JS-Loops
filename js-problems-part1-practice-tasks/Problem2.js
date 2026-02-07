d9a7022198fdad5f57a6a783f18995a917cbe222
Murad Hasan <muradhasanshuco00@gmail.com>
2026-02-07 23:18:49 +0600

    problem 1 complete

---
+++ js-problems-part1-practice-tasks/Problem2.js
@@ -0,0 +1,18 @@
+let num =  [5,6,11,12,98, 5]
+let r = []
+for( let n of num){
+    if( n === 5 ){
+    r.push(n)
+
+}
+}
+console.log(r.length)
+
+let num2 = numbers = [5,6,11,12,98, 5]
+let r2 = []
+for( let i of numbers){
+    if( i === 25){
+        r2.push(i)
+    }
+}
+console.log(r2.length)
---
+++ js-problems-part1-practice-tasks/problem.js
@@ -0,0 +1,10 @@
+
+// Task-1:
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
+function temConverter(input){
+    return input * 33.8
+}
+
+let finalResult = temConverter(2)
+console.log(finalResult ,"F")
